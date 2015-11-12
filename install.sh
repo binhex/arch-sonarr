d1a6cf46c9827b4670f007a69636a30dc491d5c4
binhex <megalith01@gmail.com>
2015-11-12 17:25:49 +0000

    ref new base image, do full pacman upgrade, use old sudo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -e
 pacman_packages="libmediainfo mono sqlite"
 
 # install pre-reqs
-pacman -Sy --noconfirm
+pacman -Syu --ignore filesystem --noconfirm
 pacman -S --needed $pacman_packages --noconfirm
 
 # call aur packer script
