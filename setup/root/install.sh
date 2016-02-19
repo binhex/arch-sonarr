8eab9ed234f6669ed16c571b56e9119564996eff
binhex <megalith01@gmail.com>
2016-02-19 21:47:35 +0000

    remove upgrade, now locked to snapshot

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -7,7 +7,6 @@ set -e
 pacman_packages="libmediainfo mono sqlite"
 
 # install pre-reqs
-pacman -Syu --ignore filesystem --noconfirm
 pacman -S --needed $pacman_packages --noconfirm
 
 # call aur packer script
