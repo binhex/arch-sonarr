db611dce3face0f5f176a2ff285178e0e65049a7
binhex <megalith01@gmail.com>
2016-09-11 22:27:50 +0100

    config supervisor for pid 1

--- setup/root/init.sh
+++ setup/root/init.sh
@@ -44,4 +44,4 @@ chmod -R 775  /usr/lib/sonarr /usr/bin/sonarr /home/nobody
 echo "[info] Starting Supervisor..."
 
 # run supervisor
-"/usr/bin/supervisord" -c "/etc/supervisor.conf" -n
+exec /usr/bin/supervisord -c /etc/supervisor.conf -n
