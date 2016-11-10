caca6e8878e78cf29e6f8da0553042fb0fa35c87
binhex <megalith01@gmail.com>
2016-11-10 13:31:07 +0000

    change base, consolidate scripts

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -3,17 +3,8 @@
 # exit script if return code != 0
 set -e
 
-# define pacman packages
-pacman_packages="base-devel git libmediainfo mono sqlite"
-
-# define aur helper
-aur_helper="apacman"
-
-# define aur packages
-aur_packages="sonarr"
-
-# install pre-reqs
-pacman -S --needed $pacman_packages --noconfirm
+# build scripts
+####
 
 # download build scripts from github
 curl -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
@@ -24,8 +15,40 @@ unzip /tmp/scripts-master.zip -d /tmp
 # move shell scripts to /root
 find /tmp/scripts-master/ -type f -name '*.sh' -exec mv -i {} /root/  \;
 
+# pacman packages
+####
+
+# define pacman packages
+pacman_packages="base-devel git libmediainfo mono sqlite"
+
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
+
+# aor packages
+####
+
+# define arch official repo (aor) packages
+aor_packages=""
+
+# call aor script (arch official repo)
+source /root/aor.sh
+
+# aur packages
+####
+
+# define aur helper
+aur_helper="apacman"
+
+# define aur packages
+aur_packages="sonarr"
+
 # call aur install script (arch user repo)
 source /root/aur.sh
+
+# container perms
+####
 
 # create file with contets of here doc
 cat <<'EOF' > /tmp/permissions_heredoc
@@ -41,6 +64,9 @@ sed -i '/# PERMISSIONS_PLACEHOLDER/{
     r /tmp/permissions_heredoc
 }' /root/init.sh
 rm /tmp/permissions_heredoc
+
+# env vars
+####
 
 # cleanup
 yes|pacman -Scc
