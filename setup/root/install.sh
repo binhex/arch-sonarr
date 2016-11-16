073541b15b648e0b286bf534d1658784218287b0
binhex <megalith01@gmail.com>
2016-11-16 13:38:06 +0000

    use base with tini, remove base-devel

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -19,7 +19,7 @@ find /tmp/scripts-master/ -type f -name '*.sh' -exec mv -i {} /root/  \;
 ####
 
 # define pacman packages
-pacman_packages="base-devel git libmediainfo mono sqlite"
+pacman_packages="git libmediainfo mono sqlite"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
