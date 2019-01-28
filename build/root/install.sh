2efea29506e34aa592eedf0d3578b44abb9d1c1e
Donald Webster <fryfrog@gmail.com>
2019-01-28 12:37:19 -0800

    A seemingly working sonarr-phantom version.

--- build/root/install.sh
+++ build/root/install.sh
@@ -30,7 +30,7 @@ fi
 ####
 
 # define aur packages
-aur_packages="sonarr"
+aur_packages="sonarr-phantom"
 
 # call aur install script (arch user repo)
 source /root/aur.sh
--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -4,4 +4,4 @@
 export XDG_CONFIG_HOME="/config/xdg"
 
 # run app
-/usr/bin/mono --debug /usr/lib/sonarr/NzbDrone.exe -nobrowser -data=/config
+/usr/bin/mono --debug /usr/lib/sonarr/Sonarr.exe -nobrowser -data=/config
